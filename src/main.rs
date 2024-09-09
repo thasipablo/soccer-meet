fe9b535a30817d9a25b0bcb68aa6cafc71f5f501
Pablo Thasi <thasipablo@gmail.com>
2024-09-09 11:50:21 +0100

    hello actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hey, Actix!");
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hello, Actix Web!")
+}
+
+#[actix_web::main]
+async fn main() ->std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
